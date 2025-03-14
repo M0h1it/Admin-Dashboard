e3f30e80c3f17ad3f5c5624921f54140b8031fb6
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-03-14 08:28:58 +0530

    final changes 1

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,16 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAhz_7F2hbLyWXxRaBrMrgj9npQ60GEj48",
   authDomain: "admin-dashboard-550f8.firebaseapp.com",
   projectId: "admin-dashboard-550f8",
-  storageBucket: "admin-dashboard-550f8.firebasestorage.app",
+  storageBucket: "admin-dashboard-550f8.appspot.com", // Fixed incorrect storageBucket URL
   messagingSenderId: "573765813307",
   appId: "1:573765813307:web:935360550b6eb68600c81e",
   measurementId: "G-8CZ9PKSKGD"
@@ -18,5 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { app, db, storage, auth, provider };
